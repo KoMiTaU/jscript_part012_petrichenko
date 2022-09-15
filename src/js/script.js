95064f8a44fd4e6564155ef127f994b5050724c1
Misha <michakohan99@gmail.com>
2022-09-15 20:54:04 +0300

    lesson 017(methods, numbers...)

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,31 @@
 "use strict";
 
-let num = 20;
+const str = "teSt";
 
-function showFirstMessage(text) {
-    console.log(text);
-    console.log(num);
-}
-
-showFirstMessage("Hello World!");
-console.log(num);
-
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(4, 7));
-// console.log(calc(6, 10));
-// console.log(calc(14, 17));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum); 
+// console.log(str[2] = 'd');
 
 
-const logger = function() {
-    console.log('Hello world')
-};
-
-logger();
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str); 
 
 
 
-const calc = (a, b) => a + b;
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 11));
+
+
+const num = 12.49;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
