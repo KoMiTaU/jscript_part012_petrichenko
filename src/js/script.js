77c8e6d0af1caf201faae1882d8d4f89c931352a
Misha <michakohan99@gmail.com>
2022-09-15 16:33:50 +0300

    lesson 015

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,69 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('How many movies you have seen?', '');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+for (let i = 0; i < 2; i++){
+    const a = prompt('Last seen movie', ''),
+          b = prompt('Your moovie score?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+
+// Второй способ с приминением while
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Last seen movie', ''),
+//           b = prompt('Your moovie score?', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         i++;
+//     } else {
+//         console.log('error');
+//     }
 // }
 
+
+// Третий способ с преминением do
+// let i = 0;
 // do {
-//     console.log(num);
-//     num++;
-// } while (num < 55);
+//     const a = prompt('Last seen movie', ''),
+//           b = prompt('Your moovie score?', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         i++;
+//         }
+//     }
+// while (i < 2);
 
-for (let i = 1; i < 10; i++) {
-    if (i ===6) {
-        break;
-    }
 
-    console.log(i);
-
+if (personalMovieDB.count < 10) {
+    console.log('Watched less films than you need!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are classic user');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are moovie expert too much films');
+} else {
+    console.log('Error');
 }
+
+
+
+console.log(personalMovieDB); 
