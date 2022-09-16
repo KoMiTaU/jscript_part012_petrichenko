fea22b1acb73aad5587f3034a3bc7531641b25c5
Misha <michakohan99@gmail.com>
2022-09-16 16:46:43 +0300

    Lesson 019 callback

--- src/js/script.js
+++ src/js/script.js
@@ -1,74 +1,27 @@
 "use strict";
 
-
-let num = 50;
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = prompt('How many movies you have seen?', '');
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('How many movies you have seen?', '');
-
-    }
+function first() {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-start();
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++){
-        const a = prompt('Last seen movie', ''),
-              b = prompt('Your moovie score?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+function second() {
+    console.log(2);
 }
 
-rememberMyFilms();
+first();
+second();
 
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Watched less films than you need!');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('You are classic user');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('You are moovie expert too much films');
-    } else {
-        console.log('Error');
-    }
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`)
+    callback();
 }
 
-detectPersonalLevel();
+function done() {
+    console.log('I pass this lesson!');
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
 }
 
-showMyDB(personalMovieDB.private);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Number of favorite movie genre ${i}`);
-    }
-}
-
-writeYourGenres();
+learnJS('JavaScript', done);
