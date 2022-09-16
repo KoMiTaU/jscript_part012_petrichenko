94bbb2d22676f49de8ec15f4ecb043351a81004e
Misha <michakohan99@gmail.com>
2022-09-16 17:43:49 +0300

    Lesson 020 objects

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,47 @@
 "use strict";
 
-function first() {
-    //Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
 
-function second() {
-    console.log(2);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
 
-first();
-second();
+options.makeTest();
 
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`)
-    callback();
-}
+const {border, bg} = options.colors;
+console.log(border)
 
-function done() {
-    console.log('I pass this lesson!');
+// console.log(Object.keys(options).length)
 
-}
+// console.log(options['colors']['border']);
 
-learnJS('JavaScript', done);
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]){
+//             console.log(`Option ${i}, gain value ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Option ${key}, gain value ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+
